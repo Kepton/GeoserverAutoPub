de9f79008c38bce708b7fb5f3977fbf8d1f46ad4
Kepton <liuyalongscu@foxmail.com>
2015-05-25 20:06:57 +0800

    第一次提交

---
+++ GeoserverAutoPub/SysParam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoserverAutoPub
+{
+    public static  class SysParam
+    {
+        /// <summary>
+        /// Geoserver的Data的路径
+        /// </summary>
+        public static string GeoserverDataPath = "";
+
+        public static string DataSource = "";
+
+        public static string UserName = "";
+
+        public static string Password = "";
+
+    }
+}
